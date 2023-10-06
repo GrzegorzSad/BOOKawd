47d289455b6079834e9af66cb93467b070fc3af0
Grzegorz Sadowski (Student) <N00223252@iadt.ie>
2023-10-06 12:07:16 +0100

    tes

---
+++ database/seeders/BooksTableSeeder.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Seeder;
+use App\Models\Book;
+
+class BooksTableSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    public function run(): void
+    {
+        Book::factory(10)->create();
+    }
+}
