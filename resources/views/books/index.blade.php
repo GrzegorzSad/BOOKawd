dfbaeb17821710a2740eadaff57d28772658b86c
Grzegorz Sadowski (Student) <N00223252@iadt.ie>
2023-10-09 14:24:42 +0100

    DOESNTWORK

---
+++ resources/views/books/index.blade.php
@@ -0,0 +1,36 @@
+<html>
+
+@extends('layouts.app')
+
+@section('content')
+<div class='container'>
+    <h1>All Books</h1>
+    <table class='table'>
+        <thead>
+            <tr>
+                <th>Title</th>
+                <th>Description</th>
+                <th>Image</th>
+            </tr>
+    </thead>
+    <tbody>
+        @foreach($books as $book)
+        <tr>
+            <td>{{$book->title}}</td>
+            <td>{{$book->title}}</td>
+            <td>
+                @if ($book->book_image)
+                    <img src="{{ $book->book_image }}"
+                    alt="{{ $book->title }}" width="100">
+                @else
+                    No Image
+                @endif
+            </td>
+        </tr>
+        @endforeach
+    </tbody>
+</table>D
+</div>
+@endsection
+
+</html>
--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,7 @@
 <?php
 
+use App\Http\Controllers\BookController;
+
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -13,6 +15,34 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
+// Route::get('/', function () {
+//     return view('welcome');
+// });
+
+
+
+Route::resource('/books',BookController::class);
+
+
+// Route::resource('books', 'BookController'); //old version dont use
+
+
+// //create a new book
+// Route::get('/books/create','BookController@create');
+// Route::post('/books','BookController@store');
+
+// //list all books
+// Route::get('/books','BookController@index');
+
+// //show a specific book
+// Route::get('/books/{book}','BookController@show');
+
+// //edit
+// Route::get('/books/{book}/edit','BookController@edit');
+// Route::put('/books/{book}','BookController@update');
+// Route::patch('/books/{book}','BookController@update');
+
+// //detete
+// Route::delete('/books/{book}','BookController@destroy');
+
+?>
